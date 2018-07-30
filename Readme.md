ddfebb0bb63616a509d302ea9988f6154602ab17
Joshua Kolden <joshua@avalanche.io>
2018-07-30 10:15:19 -0700

    initial commit

---
+++ Readme.md
@@ -0,0 +1,163 @@
+# inode - filesystem primitives for go
+The `inode` package provides `Inode`, `Directory`, and `DirEntry` primitives
+for inode style filesystems. This package does not implement any actual data
+storage features, only the data structures and logic needed to implement a
+storage system.
+
+```go
+import "github.com/absfs/inode"
+```
+
+## Usage
+See "github.com/absfs/memfs" for an example of usage in the creation of an in
+memory filesystem.
+
+The basic idea is to use the `inode` package to handle file path resolution,
+and directory entries. The use the inode number `Ino` to point to custom
+data structures in the client side package.  Here's a simplified example using
+a slice of `[]byte` for file data storage.
+
+```go
+
+type SimpleFS struct {
+  Ino  *inode.Ino
+  Root *inode.Inode
+  Data map[uint64][]byte
+}
+
+type SimpleFile struct {
+  fs       *SimpleFS
+  readonly bool
+  node     *inode.Inode
+  name     string
+  data     []byte
+}
+
+func (fs *SimpleFS) Mkdir(name string, mode os.FileMode) error {
+  child, err := fs.Root.Resolve(name)
+  if err == nil {
+    return os.ErrExist
+  }
+
+  dir, filename := filepath.Split(name)
+  dir = filepath.Clean(dir)
+
+  parent, err := fs.Root.Resolve(dir)
+  if err != nil {
+    return err
+  }
+
+  child = fs.Ino.NewDir(mode)
+  parent.Link(filename, child)
+  child.Link("..", parent)
+  return nil
+}
+
+func (fs *SimpleFS) Create(name string) (f *SimpleFile, err error) {
+  node, err := fs.Root.Resolve(name)
+  if node != nil && err == nil {
+
+    return &SimpleFile{
+      fs:   fs,
+      name: name,
+      node: node,
+      data: fs.Data[node.Ino],
+    }, nil
+  }
+
+  dir, filename := filepath.Split(name)
+  dir = filepath.Clean(dir)
+  parent, err := fs.Root.Resolve(dir)
+  if err != nil {
+    return nil, err
+  }
+
+  node = fs.Ino.New(0644)
+  fs.Data[node.Ino] = []byte{}
+  parent.Link(filename, node)
+  node.Link("..", parent)
+  return &SimpleFile{
+    fs:   fs,
+    name: name,
+    node: node,
+    data: fs.Data[node.Ino],
+  }, nil
+}
+
+func (fs *SimpleFS) Open(name string) (f *SimpleFile, err error) {
+  node, err := fs.Root.Resolve(name)
+  if err != nil {
+    return nil, err
+  }
+
+  return &SimpleFile{
+    fs:       fs,
+    readonly: true,
+    name:     name,
+    node:     node,
+    data:     fs.Data[node.Ino],
+  }, nil
+}
+
+func (f *SimpleFile) Write(p []byte) (n int, err error) {
+  if f.readonly {
+    return 0, os.ErrPermission
+  }
+
+  f.data = append(f.data, p...)
+  return len(p), nil
+}
+
+func (f *SimpleFile) Read(p []byte) (n int, err error) {
+  n = copy(p, f.data)
+  return n, nil
+}
+
+func (f *SimpleFile) Close() error {
+  if !f.readonly {
+    f.fs.Data[f.node.Ino] = f.data
+  }
+  return nil
+}
+
+func main() {
+
+  ino := new(inode.Ino)
+  fs := &SimpleFS{
+    Ino:  ino,
+    Root: ino.NewDir(0755),
+    Data: make(map[uint64][]byte),
+  }
+  err := fs.Mkdir("/tmp", 0755)
+  if err != nil {
+    panic(err)
+  }
+  f, err := fs.Create("/tmp/testfile.txt")
+  if err != nil {
+    panic(err)
+  }
+
+  n, err := f.Write([]byte("Hello, World!\n"))
+  if err != nil {
+    panic(err)
+  }
+  f.Close()
+
+  f, err = fs.Open("/tmp/testfile.txt")
+  if err != nil {
+    panic(err)
+  }
+
+  buffer := make([]byte, 512)
+  n, err = f.Read(buffer)
+  if err != nil {
+    panic(err)
+  }
+  buffer = buffer[:n]
+  if string(buffer) != "Hello, World!\n" {
+    fmt.Fprintf(os.Stderr, "error buffer == %q\n", string(buffer))
+  }
+  fmt.Printf("%s", string(buffer))
+}
+
+```
---
+++ inode.go
@@ -0,0 +1,224 @@
+package inode
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+	"syscall"
+	"time"
+)
+
+// An Inode represents the basic metadata of a file.
+type Inode struct {
+	Ino   uint64
+	Mode  os.FileMode
+	Nlink uint64
+
+	Ctime time.Time // creation time
+	Atime time.Time // access time
+	Mtime time.Time // modification time
+	Uid   uint32
+	Gid   uint32
+
+	Dir Directory
+}
+
+type DirEntry struct {
+	Name  string
+	Inode *Inode
+}
+
+func (e *DirEntry) IsDir() bool {
+	if e.Inode == nil {
+		return false
+	}
+	return e.Inode.IsDir()
+}
+
+func (e *DirEntry) String() string {
+	nodeStr := "(nil)"
+	if e.Inode != nil {
+		nodeStr = fmt.Sprintf("{Ino:%d ...}", e.Inode.Ino)
+	}
+	return fmt.Sprintf("entry{%q, inode%s", e.Name, nodeStr)
+}
+
+type Directory []*DirEntry
+
+func (d Directory) Len() int           { return len(d) }
+func (d Directory) Swap(i, j int)      { d[i], d[j] = d[j], d[i] }
+func (d Directory) Less(i, j int) bool { return d[i].Name < d[j].Name }
+
+func (n *Inode) String() string {
+	if n == nil {
+		return "<nil>"
+	}
+
+	list := make([]string, len(n.Dir))
+	for i, e := range n.Dir {
+		list[i] = e.String()
+	}
+	return fmt.Sprintf("Inode{Ino:%d,Mode:%s,Nlink:%d}\n\t%s", n.Ino, n.Mode, n.Nlink, strings.Join(list, ",\n"))
+}
+
+type Ino uint64
+
+func (n *Ino) New(mode os.FileMode) *Inode {
+	*n++
+	now := time.Now()
+	return &Inode{
+		Ino:   uint64(*n),
+		Atime: now,
+		Mtime: now,
+		Ctime: now,
+		Mode:  mode &^ os.ModeType,
+	}
+}
+
+func (n *Ino) NewDir(mode os.FileMode) *Inode {
+	dir := n.New(mode)
+	var err error
+	dir.Mode = os.ModeDir | mode&^os.ModeType
+	err = dir.Link(".", dir)
+	if err != nil {
+		panic(err)
+	}
+	err = dir.Link("..", dir)
+	if err != nil {
+		panic(err)
+	}
+
+	return dir
+}
+
+func (n *Inode) Link(name string, child *Inode) error {
+	if !n.IsDir() {
+		return errors.New("not a directory")
+	}
+
+	x := n.find(name)
+
+	entry := &DirEntry{name, child}
+
+	if x < len(n.Dir) && n.Dir[x].Name == name {
+		n.linkswapi(x, entry)
+		return nil
+	}
+	n.linki(x, entry)
+	return nil
+}
+
+func (n *Inode) Unlink(name string) error {
+
+	if !n.IsDir() {
+		return errors.New("not a directory")
+	}
+
+	x := n.find(name)
+
+	if x == n.Dir.Len() || n.Dir[x].Name != name {
+		return syscall.ENOENT // os.ErrNotExist
+	}
+
+	n.unlinki(x)
+	return nil
+}
+
+func (n *Inode) UnlinkAll() {
+	for _, e := range n.Dir {
+		if e.Name == ".." {
+			continue
+		}
+		if e.Inode.Ino == n.Ino {
+			e.Inode.countDown()
+			continue
+		}
+		e.Inode.UnlinkAll()
+		e.Inode.countDown()
+	}
+	n.Dir = n.Dir[:0]
+}
+
+func (n *Inode) IsDir() bool {
+	return os.ModeDir&n.Mode != 0
+}
+
+func (n *Inode) Resolve(path string) (*Inode, error) {
+	name, trim := popPath(path)
+	if name == "/" {
+		if trim == "" {
+			return n, nil
+		}
+		nn, err := n.Resolve(trim)
+		if err != nil {
+			return nil, err
+		}
+		if nn == nil {
+			return n, nil
+		}
+		return nn, err
+	}
+	x := n.find(name)
+	if x < len(n.Dir) && n.Dir[x].Name == name {
+		nn := n.Dir[x].Inode
+		if len(trim) == 0 {
+			return nn, nil
+		}
+		return nn.Resolve(trim)
+	}
+	return nil, syscall.ENOENT // os.ErrNotExist
+}
+
+func (n *Inode) accessed() {
+	n.Atime = time.Now()
+}
+
+func (n *Inode) modified() {
+	now := time.Now()
+	n.Atime = now
+	n.Mtime = now
+}
+
+func (n *Inode) countUp() {
+	n.Nlink++
+	n.accessed() // (I don't think link count mod counts as node mod )
+}
+
+func (n *Inode) countDown() {
+	if n.Nlink == 0 {
+		panic(fmt.Sprintf("inode %d negative link count", n.Ino))
+	}
+	n.Nlink--
+	n.accessed() // (I don't think link count mod counts as node mod )
+}
+
+func (n *Inode) unlinki(i int) {
+	n.Dir[i].Inode.countDown()
+	copy(n.Dir[i:], n.Dir[i+1:])
+	n.Dir = n.Dir[:len(n.Dir)-1]
+	n.modified()
+}
+
+func (n *Inode) linkswapi(i int, entry *DirEntry) {
+	n.Dir[i].Inode.countDown()
+	n.Dir[i] = entry
+	n.Dir[i].Inode.countUp()
+	n.modified()
+}
+
+func (n *Inode) linki(i int, entry *DirEntry) {
+	n.Dir = append(n.Dir, nil)
+	copy(n.Dir[i+1:], n.Dir[i:])
+
+	n.Dir[i] = entry
+	n.Dir[i].Inode.countUp()
+	n.modified()
+}
+
+func (n *Inode) find(name string) int {
+	return sort.Search(len(n.Dir), func(i int) bool {
+		return n.Dir[i].Name >= name
+	})
+}
